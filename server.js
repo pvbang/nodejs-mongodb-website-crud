dfc66fef40d2b273626c1e086549815ad37cd2cc
pvbang <pvbang23092002@gmail.com>
2023-11-11 00:16:07 +0700

    update

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.set("view engine", "ejs"); 
+// app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("assets"));
 
